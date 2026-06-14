06c8832ccaeb147b7a1d0727150df9fda71358dc
jokkiemm1-spec <jokkiemm1@gmail.com>
2026-06-14 16:10:59 +0100

    Delete Plugin/ping.js

--- Plugin/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    run: async (sock, msg) => {
-        await sock.sendMessage(msg.key.remoteJid, {
-            text: "🏓 DEV JOM AI BOT is alive!"
-        });
-    }
-};
